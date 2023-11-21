431e3620da7c9256b7c0c1ee2d77775f05816b4e
carina-bugaga <carina.bugaga@gmail.com>
2023-11-21 21:23:28 +0300

    add 18-task

---
+++ 18-task/18-task.js
@@ -0,0 +1,25 @@
+/* Подсчитать максимальный объем данных, который можно записать в localStorage вашего браузера. */
+
+//Получаем элементы из DOM
+const sizeNumber = document.getElementById('size');  
+
+//Очищаем хранилище от данных
+localStorage.clear();
+
+try {    
+  //Записываем максимальное количество данных с шагом итерации - 100 КВ
+  //Берем 10240 KB = 10 MB, т.к. согласно документациям это максимальный localStorage для браузеров
+  for (i = 0; i <= 10240; i += 100) {
+    //Сохраняем данные в ключ test
+    localStorage.setItem('test', new Array(i * 1024).join('a'));
+  }
+} catch (error) {
+  //Удаляем элемент test из локального хранилища    
+  localStorage.removeItem('test');
+  //Получаем данные последней успешной итерации
+  localStorage.setItem('test', i - 50); 
+}
+
+sizeNumber.innerHTML = localStorage.getItem('test');
+//Очищаем хранилище от тестовых данных
+localStorage.clear();
